141ff837f3efb04e6cedb80e0fc68c103de961a7
0gregory0 <ds4gregory@gmail.com>
2022-09-29 19:45:03 +0300

    Hosting attempt 4

--- syclecity_project/settings.py
+++ syclecity_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config #USED TO HIDE THE SECRET KEY
 
@@ -115,7 +116,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"] #TELLS DJANGO TO LOOK WITHIN THE STATIC FOLDER
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
