49930b39bc715222fd9d9c78e47fd026b13b79f0
0gregory0 <ds4gregory@gmail.com>
2022-09-29 21:32:10 +0300

    hosting attempt 9

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.urls import path
 from .views import BikeRentingPage, LandingPage
+from django.views import static
 
 #Giving the landing page view a URL
 urlpatterns = [
     path("", LandingPage.as_view(), name = "Landing Page"),
     path("rent/", BikeRentingPage.as_view(), name = "Bike Renting Page"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
