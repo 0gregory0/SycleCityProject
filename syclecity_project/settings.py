505c41dc71fa26d71421109cfabfd18da2e83769
0gregory0 <ds4gregory@gmail.com>
2022-09-30 02:02:19 +0300

    still awake

--- syclecity_project/settings.py
+++ syclecity_project/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
