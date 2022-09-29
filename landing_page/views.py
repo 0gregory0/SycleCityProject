046f8d0b9262e09ee51879e2311f1d316109ff1e
0gregory0 <ds4gregory@gmail.com>
2022-09-30 02:56:37 +0300

    csrf

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -9,6 +8,5 @@ class LandingPage (TemplateView):
     template_name = "landing_page/index.html"
 
 #Bikes to Rent view
-@csrf_exempt
 class BikeRentingPage (TemplateView):
     template_name = "landing_page/bikestorent.html"
